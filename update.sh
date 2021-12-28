c05bd056151e3edadeb96196c6fad2a0b25026a2
ABDUL RAZACK <66562892+ABDUL174@users.noreply.github.com>
2021-12-28 23:01:53 +0530

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,52 @@
+echo -e  "
+███████╗███████╗██╗░░██╗██╗░░░██╗░█████╗░██╗░░░██╗
+██╔════╝██╔════╝██║░██╔╝╚██╗░██╔╝██╔══██╗██║░░░██║
+█████╗░░█████╗░░█████═╝░░╚████╔╝░██║░░██║██║░░░██║
+██╔══╝░░██╔══╝░░██╔═██╗░░░╚██╔╝░░██║░░██║██║░░░██║
+██║░░░░░███████╗██║░╚██╗░░░██║░░░╚█████╔╝╚██████╔╝
+╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░░╚═════╝░"
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/fekyou/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/fekyou
+
+clear
+
+mkdir fekyou
+
+clear
+
+cd fekyou
+
+clear
+
+git clone https://github.com/ABDUL174/fekyou.git
+
+clear
+
+cd fekyou
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
