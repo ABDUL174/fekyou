c1587e832769698a85b3bd00e40e7cdc75ce0a74
ABDUL RAZACK <66562892+ABDUL174@users.noreply.github.com>
2021-12-28 23:45:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,9 +14,9 @@ NC='\e[0m'
 purpal='\033[35m'
 
 !#/bin/bash
-pkg install figlet -y
+sudo apt install figlet -y
 figlet PAL3RMO
-
+echo -e "${RED}                                     TOOL CREATED BY PAL3RMO
 
 echo -e "${BLUE}                                    https://github.com/ABDUL174/fekyou ${NC}"
 
